0a9c561e4219e7ecb821dec9a314d9f80e2176e3
codewithstanley <codewithstanley@gmail.com>
2023-07-10 16:47:28 +0100

    made some changes in the js script for the interactive rating and completed the qr code component-main

---
+++ 03-interactive-rating-component-main/css/specialnotes.md
@@ -0,0 +1,7 @@
+## Possible javascript flow
+
+## create a new section with javascript using createElement
+
+## set ID attributes on the section and the new new section
+
+## add eventListener to number button to selection
---
+++ 03-interactive-rating-component-main/js/main.js
@@ -0,0 +1,65 @@
+const submitButton = document.querySelector('#submit-button');
+const listItem = document.querySelectorAll('.list-item');
+const blockSection = document.querySelector('#section-block');
+
+// console.log(submitButton);
+
+submitButton.addEventListener('click', () => {
+
+    blockSection.style.display = none;
+
+   const section = document.createElement('section');
+    section.classList.add('main-section');
+    section.setAttribute('id', 'thankMsg')
+    // console.log(section)
+
+    const img = document.createElement('img');
+    img.src = './images/illustration-thank-you.svg';
+    img.alt = 'telephone image';
+    // console.log(img);
+
+    const messageHeading = document.createElement('h3');
+    messageHeading.textContent = 'Thank you!';
+
+    const messageText = document.createElement('p');
+    messageText.textContent = 'We appreciate you taking the time to give a rating. If you ever need more support, dont hesitate to get in touch!';
+    // console.log(messageText);
+
+
+    section.appendChild(img);
+    section.appendChild(messageHeading);
+    section.appendChild(messageText);
+    
+    
+    
+});
+
+// on submit
+// select each content of list
+// create thank you section
+
+/*
+const section = document.createElement('section');
+    section.classList.add('thankyouMsg');
+    section.setAttribute('id', 'thankMsg')
+    // console.log(section)
+
+    const img = document.createElement('img');
+    img.src = './images/illustration-thank-you.svg';
+    img.alt = 'telephone image';
+    // console.log(img);
+
+    const messageHeading = document.createElement('h3');
+    messageHeading.textContent = 'Thank you!';
+
+    const messageText = document.createElement('p');
+    messageText.textContent = 'We appreciate you taking the time to give a rating. If you ever need more support, dont hesitate to get in touch!';
+    // console.log(messageText);
+
+
+    section.appendChild(img);
+    section.appendChild(messageHeading);
+    section.appendChild(messageText);
+    
+
+    console.log(section); */
